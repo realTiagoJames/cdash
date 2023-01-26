7531d61588d66d976dab283fb8f507b1afc15e7a
Tiago Lima <james.lima.devops@gmail.com>
2023-01-26 13:44:38 -0300

    criado componente photo

---
+++ src/app/components/photo/photo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+
+  title = 'cdash';
+  description = 'Meu Aplicativo Gerenciador de Compras'
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg'
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
